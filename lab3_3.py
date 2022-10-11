86a11bd3272a353f2af51a0300d5bee53e12aceb
aleshkovskijjaaa <115158943+aleshkovskijjaaa@users.noreply.github.com>
2022-10-11 14:40:15 +0300

    Лабораторная работа 3

---
+++ lab3_3.py
@@ -0,0 +1,17 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+month = 0
+
+
+def student_life(money_capital, salary, spend, increase, month):
+   while True:
+      money_capital -= spend
+      if money_capital < 0:
+         break
+      money_capital += salary
+      month += 1
+      spend *= (1+increase)
+   return month
+print(student_life(money_capital, salary, spend, increase, month))
