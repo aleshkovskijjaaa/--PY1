7e094ab7311bcd470ed3b24ccb831ce6c0486475
aleshkovskijjaaa <115158943+aleshkovskijjaaa@users.noreply.github.com>
2022-10-11 22:28:18 +0300

    Лабораторная работа 1

---
+++ lab3_1.py
@@ -0,0 +1,10 @@
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+
+result =True
+
+print(src == result)
+#True and True or False and False
+#True or False
+#True
