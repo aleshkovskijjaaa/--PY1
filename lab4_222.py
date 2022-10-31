e95cb7adbf7da1859fc4cb0e718d53b6c53119a7
aleshkovskijjaaa <115158943+aleshkovskijjaaa@users.noreply.github.com>
2022-10-31 23:08:25 +0300

    Лабораторная 4 Исправленная версия

---
+++ lab4_222.py
@@ -0,0 +1,12 @@
+def delete(list_, index=None):
+    if index is None:
+        index = -1
+
+    if index < 0 :
+        return list_[:len(list_) + index] + list_[len(list_) + index + 1:]
+    else:
+        return list_[:index] + list_[index+1:]
+
+print(delete([0, 0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
