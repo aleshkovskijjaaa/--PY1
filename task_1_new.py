6ad3b55d02aec8b557bc26a3bd71dde5dc0cddb4
aleshkovskijjaaa <115158943+aleshkovskijjaaa@users.noreply.github.com>
2022-11-15 21:42:45 +0300

    lab 5 correct

--- task_1_new.py
+++ task_1_new.py
@@ -1,12 +1,5 @@
-import random
-left_border = int(input("Введите левую границу отрезка: "))
-right_border = int(input("Введите правую границу отрезка: "))
-amount_of_numbers = int(input("Введите размер выходного списка"))
-def get_unique_list_numbers() -> list[int]:
-    spisok = [i for i in range(left_border, right_border, 1)]
-    random.shuffle(spisok, random.random) #случайная последовательность чисел в списке
-    random_values = spisok[:amount_of_numbers]
-    return random_values
-list_unique_numbers = get_unique_list_numbers()
-print(list_unique_numbers)
-print(len(list_unique_numbers) == len(set(list_unique_numbers)))
+from pprint import pprint
+# TODO решить с помощью list comprehension и распечатать его
+
+dict_one = [{'bin':bin(i), 'dec':(i), 'hex':hex(i), 'oct':oct(i)} for i in range(16)]
+pprint(dict_one)
