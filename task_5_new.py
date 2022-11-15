e0c7dd12235faa7259023d79a589ec1113067f11
aleshkovskijjaaa <115158943+aleshkovskijjaaa@users.noreply.github.com>
2022-11-15 21:44:28 +0300

    Update task_5_new.py

--- task_5_new.py
+++ task_5_new.py
@@ -1,10 +1,5 @@
-import random
-def get_random_password() -> str:
-    big_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    lit_letters = "abcdefghijklmnopqrstuvwxyz"
-    numbers = "0123456789"
-    summ = ''.join((big_letters, lit_letters, numbers)) #объединение трех строк в одну
-    a = random.sample(summ, 8)
-    itog = (''.join(a))
-    return itog
-print(get_random_password())
+from pprint import pprint
+# TODO решить с помощью list comprehension и распечатать его
+
+dict_one = [{'bin':bin(i), 'dec':(i), 'hex':hex(i), 'oct':oct(i)} for i in range(16)]
+pprint(dict_one)
